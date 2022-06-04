05030181a48a4d9d8ddf4ddeff2b1d0d2e6f9ad8
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-06-04 18:05:47 +0900

    init()

---
+++ spring-trader/src/main/java/com/example/springtrader/SpringTraderApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTraderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTraderApplication.class, args);
+    }
+
+}
