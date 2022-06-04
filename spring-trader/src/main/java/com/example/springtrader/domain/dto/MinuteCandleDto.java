9c3c27ab33b55ea70660f73d114a6de2ae893b42
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-06-04 19:38:45 +0900

    feat(MinuteCandleDto), refacto

---
+++ spring-trader/src/main/java/com/example/springtrader/domain/dto/MinuteCandleDto.java
@@ -0,0 +1,52 @@
+package com.example.springtrader.domain.dto;
+
+import com.example.springtrader.domain.entity.MinuteCandle;
+import com.example.springtrader.domain.enums.MarketType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MinuteCandleDto {
+
+    @JsonProperty("market")
+    private String market;
+
+    @JsonProperty("candle_date_time_utc")
+    private LocalDateTime candleDateTimeUtc;
+
+    @JsonProperty("candle_date_time_kst")
+    private LocalDateTime candleDateTimeKst;
+
+    @JsonProperty("opening_price")
+    private Double openingPrice;
+
+    @JsonProperty("high_price")
+    private Double highPrice;
+
+    @JsonProperty("low_price")
+    private Double lowPrice;
+
+    @JsonProperty("trade_price")
+    private Double tradePrice;
+
+    private Long timestamp;
+
+    @JsonProperty("candle_acc_trade_price")
+    private Double candleAccTradePrice;
+
+    @JsonProperty("candle_acc_trade_volume")
+    private Double candleAccTradeVolume;
+
+    public MinuteCandle toMinuteCandle() {
+        return new MinuteCandle(
+                MarketType.find(market), openingPrice, highPrice, lowPrice, tradePrice, candleAccTradePrice,
+                candleAccTradeVolume, candleDateTimeUtc, candleDateTimeKst, timestamp
+        );
+    }
+}
