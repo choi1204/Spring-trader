301caa4c21f248af2461a83cf338eb4664bd42df
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-06-05 04:40:51 +0900

    feat(ModeProperty)

---
+++ spring-trader/src/main/java/com/example/springtrader/config/properties/ModeProperties.java
@@ -0,0 +1,14 @@
+package com.example.springtrader.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties(prefix = "mode")
+@Configuration
+@Getter
+@Setter
+public class ModeProperties {
+    private String mode;
+}
