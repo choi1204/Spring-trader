a32a6ac0b807f8c1d81891dac247c668bce5f839
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-06-04 18:06:45 +0900

    test((UpbitClient)

---
+++ spring-trader/src/test/java/com/example/springtrader/SpringTraderApplicationTests.java
@@ -0,0 +1,37 @@
+package com.example.springtrader;
+
+import com.trader.common.enums.MarketUnit;
+import com.trader.common.enums.MinuteType;
+import com.trader.common.utils.MinuteCandle;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest
+@PropertySource(
+        value = {"classpath:application.yaml"},
+        factory = YamlLoadFactory.class)
+class SpringTraderApplicationTests {
+
+    @Autowired
+    UpbitCandleClient upbitCandleClient;
+
+    @Autowired
+    UpbitProperties upbitProperties;
+
+    @Test
+    void contextLoads() {
+
+        List<MinuteCandle> minuteCandles = upbitCandleClient.getMinuteCandles(MinuteType.FIVE, MarketUnit.KRW, 200, LocalDateTime.now());
+        assertThat(minuteCandles.size()).isEqualTo(200);
+    }
+
+}
