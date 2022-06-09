c9eae43e49bcf841190936291dcfa772eb3ed2d7
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-06-09 23:14:10 +0900

    feat(CoinWallet)

---
+++ spring-trader/src/main/java/com/example/springtrader/common/domain/dto/CoinWalletDto.java
@@ -0,0 +1,36 @@
+package com.example.springtrader.common.domain.dto;
+
+import com.example.springtrader.common.domain.entity.CoinWallet;
+import com.example.springtrader.common.enums.CurrencyType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Currency;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoinWalletDto {
+
+    private String currency;
+
+    private Double balance;
+
+    private Double locked;
+
+    @JsonProperty("avg_buy_price")
+    private Double avgBuyPrice;
+
+    @JsonProperty("avg_buy_price_modified")
+    private Boolean avgBuyPriceModified;
+
+    @JsonProperty("unit_currency")
+    private String unitCurrency;
+
+    public CoinWallet toCoinWallet() {
+        CurrencyType currencyType = CurrencyType.find(currency);
+        return new CoinWallet(currencyType, balance, avgBuyPrice);
+    }
+}
