328b9a99e6142f599e1b6ffcf04068f558b28cf0
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-06-14 21:44:28 +0900

    feat(CoinWallet) : 필드 추가

--- spring-trader/src/main/java/com/example/springtrader/common/domain/entity/CoinWallet.java
+++ spring-trader/src/main/java/com/example/springtrader/common/domain/entity/CoinWallet.java
@@ -9,10 +9,9 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 @Table
 @Getter
+@NoArgsConstructor
 public class CoinWallet {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,10 +29,25 @@ public class CoinWallet {
     //매수 평균가
     private Double avgBuyPrice;
 
+    //현재 금액
+    private Double nowTotalPrice;
+
+    //현재 수익률
+    private Double nowProfitRate;
+
+    private LocalDateTime tradeTime;
+
     public CoinWallet(CurrencyType currencyType, Double balance, Double avgBuyPrice) {
         this.currencyType = currencyType;
         this.totalBidPrice = avgBuyPrice * balance;
         this.balance = balance;
         this.avgBuyPrice = avgBuyPrice;
     }
+
+
+    public void update(Double nowPrice) {
+        this.nowTotalPrice = nowPrice * balance;
+        this.nowProfitRate = nowTotalPrice / totalBidPrice;
+    }
+
 }
